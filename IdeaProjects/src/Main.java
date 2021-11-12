5484cbafbada8cb270e3e3e9ecf37283e665e73d
Roseroose <2200660@learn.vsb.bc.ca>
2021-11-12 15:02:29 -0800

    nj

---
+++ IdeaProjects/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        //Primitive data types
+        /*
+        int
+        double
+        char
+        boolean
+        */
+        System. out. println("No");
+    }
+}
+
