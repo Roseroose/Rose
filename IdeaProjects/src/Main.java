c6aa6de1a991e6cb0c7850f4df7b1884ac4fc955
Roseroose <2200660@learn.vsb.bc.ca>
2021-11-12 15:04:58 -0800

    variable practice

--- IdeaProjects/src/Main.java
+++ IdeaProjects/src/Main.java
@@ -1,13 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        //Primitive data types
-        /*
-        int
-        double
-        char
-        boolean
-        */
-        System. out. println("No");
+
+        System.out. println(-2);
+        System.out. println(-2*3+4);
+        System.out.println((-3%4)+1);
+        System.out.println(Math.pow(2,2)-(2*3));
+        System.out.println((9 % 4) - 8 + Math.pow(3,2) / 9 + (Math.sqrt(144) / 3));
+        System.out. println(Math.cos(2 * Math.PI)-Math.pow (3,3)/9);
     }
 }
-
