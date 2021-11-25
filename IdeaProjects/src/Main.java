9805c741168425a7da20c953f897be809eb3dfb1
Roseroose <94019412+Roseroose@users.noreply.github.com>
2021-11-25 13:52:12 -0800

    Update Main.java

--- IdeaProjects/src/Main.java
+++ IdeaProjects/src/Main.java
@@ -1,11 +1,28 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out. println(-2);
-        System.out. println(-2*3+4);
-        System.out.println((-3%4)+1);
-        System.out.println(Math.pow(2,2)-(2*3));
-        System.out.println((9 % 4) - 8 + Math.pow(3,2) / 9 + (Math.sqrt(144) / 3));
-        System.out. println(Math.cos(2 * Math.PI)-Math.pow (3,3)/9);
+        System.out.println((9 - 4)+ 10/2 - (4 - 8)/3);
+        System.out.println(5+5-(-4)/3);
+        System.out.println(10-(-1));
+        System.out.println(11);
+
+        System.out.println(23%4 + (18 - 3 / 2));
+        System.out.println(3+(18-1));
+        System.out.println(3+17);
+        System.out.println(20);
+
+        System.out.println(((8 - 5) + Math.pow(3,4)/5));
+        System.out.println(((3) + 81.0/5));
+        System.out.println(3 + 16.2);
+        System.out.println(19.2);
+
+        System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
+        System.out.println(1-8+8.0/8+9.0/3);
+        System.out.println(1-8+1.0+3.0);
+        System.out.println(-3.0);
+
+        System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
+        System.out.println(((7 + 3) * Math.sin(45) + 2) - Math.pow(5, 3));
+        System.out.println((10 * Math.sin(45) + 2) - 125.0);
     }
 }
