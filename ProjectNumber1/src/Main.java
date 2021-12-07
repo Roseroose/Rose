a3a317158577fe0adec3680b01dac481a7378d17
Roseroose <94019412+Roseroose@users.noreply.github.com>
2021-12-07 15:01:02 -0800

    Update Main.java

--- ProjectNumber1/src/Main.java
+++ ProjectNumber1/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     //Code your solution to problem number one here
     static int problemOne(String s){
-        int answer = 0;
+        int count = 0;
 
         for(int i = 0;i < s.length();i++){
             switch (s.charAt(i)){
@@ -11,73 +11,60 @@ public class Main {
                 case 'i':
                 case 'o':
                 case 'u':
-                    answer++;
+                    count++;
                     break;
             }
         }
 
-        return answer;
+        return count;
     }
     //Code you problem number two here
     static int problemTwo(String s){
-        int answer = 0;
+        int count = 0;
 
-        //tests each substring of 3 to see if it matches "bob"
+
         for (int i = 0;i <= s.length() - 3;i++){
             String substring = s.substring(i,i+3);
             if (substring.equals("bob"))
-                answer++;
+                count++;
         }
 
-        return answer;
+        return count;
     }
     //Code your solution to problem number 3 here
     static String problemThree(String s){
-        //establishing the alphabet as a string
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-        //variable initializing
-        int alphabetPosA = 0;
-        int alphabetPosB = 0;
-        int successCount = 0;
-        String tempCandidate;
+        int A = 0;
+        int B = 0;
+        int success = 0;
+        String temp;
         String bestCandidate = s.substring(0,1);
 
-        /*Main Loop
-        checks if i and i+1 are in order
-        on success updates temp candidate and if temp candidate is longer updates best candidate
-         */
         for (int i = 0;i < s.length() - 1;i++) {
-            for (int j = 0; j < 26; j++) {
-                if (s.charAt(i) == alphabet.charAt(j)) {
-                    alphabetPosA = j;
+            for (int x = 0; x < 26; x++) {
+                if (s.charAt(i) == alphabet.charAt(x)) {
+                    A = x;
                 }
-                if (s.charAt(i + 1) == alphabet.charAt(j)) {
-                    alphabetPosB = j;
+                if (s.charAt(i + 1) == alphabet.charAt(x)) {
+                    B = x;
                 }
             }
-            if (alphabetPosA <= alphabetPosB) {
-                tempCandidate = s.substring(i - successCount, i + 2);
-                if (tempCandidate.length() > bestCandidate.length()) bestCandidate = tempCandidate;
-                successCount++;
+            if (A <= B) {
+                temp = s.substring(i - success, i + 2);
+                if (temp.length() > bestCandidate.length()) bestCandidate = temp;
+                success++;
             } else {
-                successCount = 0;
+                success = 0;
             }
         }
         return bestCandidate;
     }
     public static void main(String[] args) {
-        /*
-        Set s to a string and run your method using s as the parameter
-        Run your method in a println statement to determine what the output was
-        Once you think you have it working try running the tests.
-        The tests will put your method through several different Strings to test
-        all possible cases.  If you have 100% success then there is no bugs in your methods.
-         */
+
         Scanner scan = new Scanner(System.in);
-        System.out.println("what string?\n");
+        System.out.println("Please enter the string:");
         String s = scan.nextLine();
-        System.out.println("\nNumber of vowels: " + problemOne(s));
+        System.out.println("Number of vowels: " + problemOne(s));
         System.out.println("Number of times bob occurs is: " + problemTwo(s));
         System.out.println("Longest substring in alphabetical order is: " + problemThree(s));
     }
